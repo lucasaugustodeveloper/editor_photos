120c4606d6247704311ceb846ff02aa5592c60e3
Lucas Augusto <lucas.augusto5061@gmail.com>
2017-07-12 11:15:34 -0300

    adicionado sass, criando gulpfile para gerenciar as tarefas do projeto

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp')
+const bs = require('browser-sync')
+const sass = require('gulp-sass')
+const prefixer = require('gulp-autoprefixer')
+
+const paths = {
+	css: ['assets/stylesheets'],
+	img: ['assets/images'],
+	js: ['assets/javascripts'],
+	sass: ['assets/sass']
+}
+
+gulp.task('default', () => {})
+
+gulp.task('server', () => {
+	bs.init({
+		server: {
+			baseDir: './'
+		},
+		open: false,
+		reloadOnRestart: true
+	})
+
+	gulp.watch('index.html', bs.reload)
+	gulp.watch(`${paths.css}/**/*.css`, bs.reload)
+	gulp.watch(`${paths.sass}/**/*.scss`, ['sass'])
+	gulp.watch(`${paths.js}/**/*.js`, bs.reload)
+})
+
+gulp.task('sass', () => {
+	return gulp.src(`${paths.sass}/**/*.scss`)
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		.pipe(prefixer({
+			browsers: ['last 10 version', 'ie 10', 'ie 9'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('assets/stylesheets'))
+})

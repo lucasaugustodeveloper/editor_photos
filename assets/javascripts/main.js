a51c455397eeab5b5e08d0a7cc54b5e60f46144b
Lucas Augusto <lucas.augusto5061@gmail.com>
2017-07-18 15:20:28 -0300

    refatorado código, para desenha somente uma forma por vez

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,5 +1,5 @@
 const screenSize = {
-  width: document.querySelector('.screen').clientWidth,
+  width: document.querySelector('.screen').clientWidth - 17,
   height: 900
 }
 const config = (id, width, height) => {
@@ -24,94 +24,98 @@ const mousePosition = (canvas, evt) => {
     obj = obj.offsetParent
   }
 
-  let mouseX = (evt.clientX - (left + window.pageXOffset)) - 2.5
-  let mouseY = (evt.clientY - (top + window.pageYOffset)) - 2.5
+  let mouseX = (evt.clientX - (left + window.pageXOffset))
+  let mouseY = (evt.clientY - (top + window.pageYOffset))
 
   return {
     x: mouseX,
     y: mouseY
   }
 }
-const draws = (data) => {
-  const drawing = []
-  drawing.push(data)
-  console.log(drawing)
-}
 const drawFree = (canvas, context) => {
-  let drawing = false
+  let desenho = false
 
   canvas.onmousedown = (evt) => {
+    if (drawing !== 'free') return
+
     context.moveTo(evt.clientX, evt.clientY)
-    drawing = true
+    desenho = true
   }
   canvas.onmouseup = () => {
-    drawing = false
+    return desenho = false
   }
   canvas.onmousemove = (evt) => {
-    if (drawing) {
+    if (desenho && drawing === 'free') {
       context.lineTo(evt.clientX, evt.clientY)
       context.stroke()
     }
   }
 }
-const drawPin = (canvas, context, evt) => {
+const drawPin = (context, x, y) => {
   const pin = []
-  canvas.addEventListener('click', (e) => {
-    const mousePos = mousePosition(canvas, e)
-    context.fillRect(`${mousePos.x}`, `${mousePos.y}`, 10, 10)
+
+    context.save()
+    context.translate (x, y)
+
+    context.beginPath()
+    context.moveTo(0, 0)
+    context.bezierCurveTo(2, -10, -20, -25, 0, -30)
+    context.bezierCurveTo(20, -25, -2, -10, 0, 0)
+    context.fillStyle = 'orange'
+    context.fill()
+    context.strokeStyle = 'black'
+    context.lineWidth = 1.5
+    context.stroke()
+    context.beginPath()
+    context.arc(0, -21, 3, 0, Math.PI * 2)
+    context.closePath()
+    context.fillStyle = 'black'
+    context.fill()
+    context.restore()
     pin.push({
       drawPin: {
-        x: mousePos.x,
-        y: mousePos.y
+        x: x,
+        y: y
       }
     })
-    console.log(pin)
-  })
+    // console.log(pin)
 }
-const drawCircle = (canvas, context, radius, evt) => {
+const drawCircle = (context, x, y, radius, evt) => {
   const circle = []
-  canvas.addEventListener('click', (evt) => {
-    const mousePos = mousePosition(canvas, evt)
-    context.beginPath()
-    context.arc(`${mousePos.x}`, `${mousePos.y}`, `${radius}`, 0, Math.PI * 2)
-    context.closePath()
-    context.fill()
-    context.stroke()
-    circle.push({
-      circle: {
-        x: mousePos.x,
-        y: mousePos.y
-      }
-    })
+  context.beginPath()
+  context.arc(`${x + 2}`, `${y + 6}`, `${radius}`, 0, 2 * Math.PI)
+  context.closePath()
+  context.fill()
+  context.stroke()
+  circle.push({
+    circle: {
+      x: x,
+      y: y
+    }
   })
 }
-const drawSquare = (canvas, context, width, height, evt) => {
+const drawSquare = (context, x, y, width, height, evt) => {
   const square = []
-  canvas.addEventListener('click', (evt) => {
-    const mousePos = mousePosition(canvas, evt)
-    context.rect(`${mousePos.x}`, `${mousePos.y}`, `${width}`, `${height}`)
-    context.stroke()
-    square.push({
-      square: {
-        x: mousePos.x,
-        y: mousePos.y
-      }
-    })
-    console.log(square)
+  context.beginPath()
+  context.rect(`${x - 50}`, `${y - 50}`, `${width}`, `${height}`)
+  context.closePath()
+  context.fill()
+  context.stroke()
+  square.push({
+    square: {
+      x: x,
+      y: y
+    }
   })
 }
-const drawText = (canvas, context, text) => {
+const drawText = (text, x, y) => {
   const contentText = []
-  canvas.addEventListener('click', (evt) => {
-    const mousePos = mousePosition(canvas, evt)
-    context.font = '2rem Monaco'
-    context.fillText(`${text}`, `${mousePos.x - 15}`, `${mousePos.y + 15}`)
-    contentText.push({
-      content: text,
-      x: mousePos.x,
-      y: mousePos.y
-    })
-    console.log(contentText)
+  context.font = '2rem Monaco'
+  context.fillText(`${text}`, `${x - 15}`, `${y + 15}`)
+  contentText.push({
+    content: text,
+    x: x,
+    y: y
   })
 }
 const saveCanvas = (canvas, id) => {
@@ -126,8 +130,13 @@ const saveCanvas = (canvas, id) => {
     return false
   });
 }
-const clearCanvas = (canvas) => {
+const downloadCanvas = (link, canvas, filename) => {
+  link.href = document.querySelector(canvas).toDataURL()
+  link.download = filename
+}
+const clearCanvas = (canvas, context) => {
   canvas.width = canvas.width
+  context.drawImage(bgImage, 0, 0, screen.width, screen.height)
 }
 
 const canvas = config('#screen', screenSize.width, screenSize.height)
@@ -141,23 +150,55 @@ window.onload = () => {
   context.drawImage(bgImage, 0, 0, screen.width, screen.height)
 }
 
-let drawing = 'free'
+let drawing
 
-const draw = () => {
+const draw = (screen, context, x, y) => {
   switch (drawing) {
     case 'circle':
-      // circle
+      drawCircle(context, x, y, 20)
       break
     case 'square':
-      // square
-      break
-    case 'text':
-      // text
+      drawSquare(context, x, y, 100, 100)
       break
     case 'pin':
-      // pin
+      drawText('Lucas', x, y)
+      drawPin(context, x, y, 20)
+      break
+    case 'free':
+      drawFree(context, x, y)
+      break
     default:
-      // free
+      console.log('Selecione um botão para desenha')
       break
   }
 }
+
+document.querySelector('.comment').addEventListener('click', () => {
+  drawing = 'pin'
+}, true)
+document.querySelector('.form_circle').addEventListener('click', () => {
+  drawing = 'circle'
+}, true)
+document.querySelector('.form_square').addEventListener('click', () => {
+  drawing = 'square'
+}, true)
+document.querySelector('.insertText').addEventListener('click', () => {
+  drawing = 'text'
+}, true)
+document.querySelector('.draw_free').addEventListener('click', () => {
+  drawing = 'free'
+}, true)
+document.querySelector('.download').addEventListener('click', () => {
+  const btnDownload = document.querySelector('.download')
+  downloadCanvas(btnDownload, '#screen', 'test.png')
+}, false)
+document.querySelector('.clear_canvas').addEventListener('click', () => {
+  clearCanvas(screen, context)
+}, false)
+
+screen.addEventListener('click', (e) => {
+  const mousePos = mousePosition(screen, e)
+  draw(screen, context, mousePos.x, mousePos.y)
+}, true)
+
+
